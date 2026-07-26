2882bc6d819a58e4bc084b368a963f457aeb7e9a
karlbifu <aginetdirectiontechnique@gmail.com>
2026-07-27 00:59:40 +0100

    second commit

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // Nom du dépôt GitHub -> le site sera servi depuis /karlbifu/
+  base: '/karlbifu/',
 })
